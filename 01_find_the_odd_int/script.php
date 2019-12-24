48fd55a836e2723cdfa9523d069c13a8168e73d4
Fraser Tooth <frasertooth@gmail.com>
2019-12-24 18:03:40 +0900

    Find the odd int

---
+++ 01_find_the_odd_int/script.php
@@ -0,0 +1,20 @@
+function findIt(array $seq) : int
+{
+    $map=array();
+    foreach ($seq as $num) {
+      if (array_key_exists($num, $map)) { 
+        $map[$num] += 1;
+      } 
+      else{ 
+        $map[$num] = 1;
+      } 
+    };
+    
+    foreach ($map as $num => $times) {
+      if($times%2){
+        return $num;
+      }
+    };
+    
+    return false;
+}
